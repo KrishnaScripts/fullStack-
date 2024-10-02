73881f0aa131d30682b99c85cf62c10f34317856
KrishnaScripts <krishnahpatel2000@gmail.com>
2024-10-02 13:15:10 +0100

    check test API

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,18 @@
-import express from 'express';
 
+import express, { Request, Response } from "express";
+import cors from "cors";
 const app = express();
 
 const port = 3000;
 
+app.use(express.json());
+app.use(cors());
+app.get("/api/sample", (request:Request,response:Response)=>{
+    response.json({message: "Hello from Backend!"});
+})
+
 app.listen(port,()=>{
     console.log(`running port on ${port}`);
 });
+
+
